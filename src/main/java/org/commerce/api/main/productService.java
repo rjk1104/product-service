2ad0abd17556c3d19db2af793244c3d7e21794bf
Rimjhim kaurav <kaurimjhim@gmail.com>
2022-05-28 15:07:53 +0530

    Adding product Service

---
+++ src/main/java/org/commerce/api/main/productService.java
@@ -0,0 +1,12 @@
+package org.commerce.api.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class productService {
+    public static void main(String[] args) {
+        SpringApplication.run(productService.class,args);
+    }
+}
+
