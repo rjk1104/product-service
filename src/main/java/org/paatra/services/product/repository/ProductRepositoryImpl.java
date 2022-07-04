7b254bff250a00531bd8bae13937288f0a9b49f8
Anadi Nema <nema.anadi99@gmail.com>
2022-07-04 23:18:58 +0530

    Baseline code for the product service

---
+++ src/main/java/org/paatra/services/product/repository/ProductRepositoryImpl.java
@@ -0,0 +1,13 @@
+//package org.paatra.services.product.repository;
+//
+//import org.paatra.services.product.entity.Product;
+//import org.paatra.services.product.util.ProductEntity_pk;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public class ProductRepositoryImpl implements ProductRepository{
+//    public List<Product> findAllByCollection() {
+//        return null;
+//    }
+//}
